d64f8c1e41e30f43dfd81519efa6d1c819e18dac
priyareddy1404@gmail.com <priyareddy1404@gmail.com>
2026-07-20 17:00:27 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ app.delete("/users" , (req, res) => {
 //  res.send("Hello hello hello");
 // });
 
+//Query Parameters and Dynamic Routes
+
 app.get("user/:userId/:name/:password" ,(req,res) => {
     console.log(req.params);   //Read dynamic route
     console.log(req.query);  //Read data
@@ -37,3 +39,30 @@ app.get("user/:userId/:name/:password" ,(req,res) => {
 app.listen(3000 , () => {
     console.log("Server is successfully listening on port 3000");
 }); 
+
+
+//Multiple Route Handlers
+
+app.get("/users" , 
+    
+(req, res ,next) => {
+    console.log("Handling the route user");
+    // res.send("Hnadling routes");
+    next();     //next() passes control to the next middleware or route handler.
+},
+
+(req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Route Handling");
+    next();
+},
+
+(req, res, next) => {
+    next();
+    // res.send("3rd Route handling");
+},
+
+(req, res) => {
+    console.log("Handling the route user 4!!");
+    res.send("4th Route Handler");
+});
